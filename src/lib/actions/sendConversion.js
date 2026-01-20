d2422056435b851c6725ba728049c00be4936372
Serban Stancu <stancu@adobe.com>
2026-01-20 14:58:10 -0700

    Update API version to 202509.

--- src/lib/actions/sendConversion.js
+++ src/lib/actions/sendConversion.js
@@ -63,7 +63,7 @@ module.exports = async ({ utils }) => {
 
   const url = 'https://api.linkedin.com/rest/conversionEvents';
   const method = 'POST';
-  const version = '202502';
+  const version = '202509';
 
   return fetch(
     url,
